9160391307f0cb5cecb4798b9de914881a972f28
Hazed56227 <bonneville.alexandre56@gmail.com>
2026-09-24 15:34:16 +0200

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,15 @@
+const firebaseConfig = {
+
+  apiKey: "AIzaSyBRRRI9kdcKxaWVJJxkgrPUMuOzjfD_k-8",
+
+  authDomain: "cahier-89ae2.firebaseapp.com",
+
+  projectId: "cahier-89ae2",
+
+  storageBucket: "cahier-89ae2.firebasestorage.app",
+
+  messagingSenderId: "165189450872",
+
+  appId: "1:165189450872:web:a8f3cc0c72e5a47b0a418d"
+
+};
